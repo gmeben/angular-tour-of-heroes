d8044c6c8c65cf398581c8c9032c1dec619209f5
gmeben <gmeben@gmail.com>
2020-07-10 22:18:33 -0400

    feat: Add HeroService injectable in HeroesComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,4 +12,5 @@ export class HeroService {
   getHeroes(): Hero[] {
     return HEROES
   }
+  
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,7 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES
+  heroes: Hero[]
 
   hero: Hero = {
     id: 1,
@@ -21,9 +22,14 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
 }
